4131ca9b977169ef70498a464158f4db200c7e27
keviny22 <kevin_young@intuit.com>
2013-05-01 15:27:12 -0700

    shell layout

---
+++ README.md
@@ -0,0 +1,13 @@
+# campfire_handler-cookbook cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: Intuit, Inc. (<kevin_young@intuit.com>)
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             "campfire_handler-cookbook"
+maintainer       "Intuit, Inc."
+maintainer_email "kevin_young@intuit.com"
+license          "Apache 2.0"
+description      "Installs/Configures campfire_handler-cookbook"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+

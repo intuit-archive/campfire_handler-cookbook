bb6c2405f10f6e23c993b6df7c2edd4f23af9ce9
Kevin Young <kevin_young@intuit.com>
2013-05-16 17:03:00 -0700

    updated readme

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,12 +1,4 @@
-## v1.0.6:
+## 0.1.0:
 
-* [COOK-1069] - typo in chef_handler readme
+* Initial version
 
-## v1.0.4:
-
-* [COOK-654] dont try and access a class before it has been loaded
-* fix bad boolean check (if vs unless)
-
-## v1.0.2:
-
-* [COOK-620] ensure handler code is reloaded during daemonized chef runs
--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
-maintainer       "Opscode, Inc."
-maintainer_email "cookbooks@opscode.com"
+maintainer       "Intuit, Inc."
+maintainer_email "kevin_young@intuit.com"
 license          "Apache 2.0"
-description      "Distribute and enable Chef Exception and Report handlers"
+description      "Handler for campfire to be used in conjunction with chef_handler"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.6"
+version          "0.1.0"

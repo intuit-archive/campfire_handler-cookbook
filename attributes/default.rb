17b96513d842088a8b440f0c59246761d1f96d1a
Kevin Young <kevin_young@intuit.com>
2013-05-21 10:43:02 -0700

    Updated attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
-default['campfire_handler']['campfire']  = 'change_me_campfire'
+default['campfire_handler']['campfire']  = {"room_id"=>"changeme_roomid", "token"=>"changeme_token", "subdomain"=>"changeme_subdomain"}
 default['campfire_handler']['stack']     = 'change_me_stack'
 default['campfire_handler']['ip']        = 'change_me_ip'

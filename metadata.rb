35600ccf126d851b1486d9fd09cb63333be36e63
capen <cbrinkley@gmail.com>
2013-07-02 15:09:31 -0700

    removing changes already present in PR2.

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,6 @@
-name             "campfire_handler"
 maintainer       "Intuit, Inc."
 maintainer_email "kevin_young@intuit.com"
 license          "Apache 2.0"
 description      "Handler for campfire to be used in conjunction with chef_handler"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
-
-%w{ chef_handler }.each do |cb|
-    depends cb
-end
